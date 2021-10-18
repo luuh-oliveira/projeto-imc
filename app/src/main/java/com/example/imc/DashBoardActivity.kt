e427fa2add0a1117af3b6a99d1dd06266d80187c
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2021-10-18 11:23:09 -0300

    Aula 18-10-2021

--- app/src/main/java/com/example/imc/DashBoardActivity.kt
+++ app/src/main/java/com/example/imc/DashBoardActivity.kt
@@ -3,9 +3,9 @@ package com.example.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ProfileDoisActivity : AppCompatActivity() {
+class DashBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_dois)
+        setContentView(R.layout.activity_dashboard)
     }
 }
--- app/src/main/java/com/example/imc/LoginActivity.kt
+++ app/src/main/java/com/example/imc/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.example.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar!!.hide()
+
+        val tvNovaConta = findViewById<TextView>(R.id.tv_nova_conta)
+
+        tvNovaConta.setOnClickListener {
+            val novaConta = Intent(this, NovoUsuarioActivity::class.java)
+            startActivity(novaConta)
+
+        }
     }
 }
